d487c565ff2754bec459b152b6c974ab19f94c18
kishan-111 <kishankr7909@gmail.com>
2023-10-03 23:15:15 +0530

    updated UNet

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -2,25 +2,33 @@
 
 from .unet_parts import *
 
+""" Full assembly of the parts to form the complete network """
 
 class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
+    def __init__(self, num_in_channels, num_classes, bilinear=False, dropout_probability=0, normalization='batchnorm',
+                 num_groups=1):
         super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
+
+        self.n_channels = num_in_channels
+        self.n_classes = num_classes
         self.bilinear = bilinear
 
-        self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
-        factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
+        self.normalization = normalization
+        self.num_groups = num_groups
+
+        self.inc = DoubleConv(self.n_channels, 64, normalization=self.normalization, num_groups=self.num_groups)
+        self.down1 = Down(64, 128, normalization=self.normalization, num_groups=self.num_groups)
+        self.down2 = Down(128, 256, normalization=self.normalization, num_groups=self.num_groups)
+        self.down3 = Down(256, 512, normalization=self.normalization, num_groups=self.num_groups)
+        factor = 2 if self.bilinear else 1
+        self.down4 = Down(512, 1024 // factor, normalization=self.normalization, num_groups=self.num_groups)
+        self.up1 = Up(1024, 512 // factor, self.bilinear, normalization=self.normalization, num_groups=self.num_groups)
+        self.up2 = Up(512, 256 // factor, self.bilinear, normalization=self.normalization, num_groups=self.num_groups)
+        self.up3 = Up(256, 128 // factor, self.bilinear, normalization=self.normalization, num_groups=self.num_groups)
+        self.up4 = Up(128, 64, self.bilinear, normalization=self.normalization, num_groups=self.num_groups)
+        self.outc = OutConv(64, self.n_classes)
+        self.softmax = nn.Softmax2d()
+        self.dropout = nn.Dropout2d(p=dropout_probability)
 
     def forward(self, x):
         x1 = self.inc(x)
@@ -28,21 +36,86 @@ class UNet(nn.Module):
         x3 = self.down2(x2)
         x4 = self.down3(x3)
         x5 = self.down4(x4)
+
         x = self.up1(x5, x4)
         x = self.up2(x, x3)
         x = self.up3(x, x2)
         x = self.up4(x, x1)
+        x = self.dropout(x)
         logits = self.outc(x)
+        # preds = self.softmax(logits)
         return logits
 
-    def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint(self.inc)
-        self.down1 = torch.utils.checkpoint(self.down1)
-        self.down2 = torch.utils.checkpoint(self.down2)
-        self.down3 = torch.utils.checkpoint(self.down3)
-        self.down4 = torch.utils.checkpoint(self.down4)
-        self.up1 = torch.utils.checkpoint(self.up1)
-        self.up2 = torch.utils.checkpoint(self.up2)
-        self.up3 = torch.utils.checkpoint(self.up3)
-        self.up4 = torch.utils.checkpoint(self.up4)
-        self.outc = torch.utils.checkpoint(self.outc)
+
+""" UNet with additional intermediate output-heads """
+
+class HierarchicalUNet(nn.Module):
+    def __init__(self, num_in_channels, num_classes, bilinear=False, dropout_probability=0.0, normalization='batchnorm',
+                 num_groups=1):
+        super(HierarchicalUNet, self).__init__()
+
+        # Basic configuration parameters
+        self.n_channels = num_in_channels
+        self.n_classes = num_classes
+        self.bilinear = bilinear
+        self.normalization = normalization
+        self.num_groups = num_groups
+        self.dropout = nn.Dropout2d(p=dropout_probability)
+
+        # Standard UNet layer definitions
+        self.inc = DoubleConv(self.n_channels, 64, normalization=self.normalization, num_groups=self.num_groups)
+        self.down1 = Down(64, 128, normalization=self.normalization, num_groups=self.num_groups)
+        self.down2 = Down(128, 256, normalization=self.normalization, num_groups=self.num_groups)
+        self.down3 = Down(256, 512, normalization=self.normalization, num_groups=self.num_groups)
+        factor = 2 if self.bilinear else 1
+        self.down4 = Down(512, 1024 // factor, normalization=self.normalization, num_groups=self.num_groups)
+        self.up1 = Up(1024, 512 // factor, self.bilinear, normalization=self.normalization, num_groups=self.num_groups)
+        self.up2 = Up(512, 256 // factor, self.bilinear, normalization=self.normalization, num_groups=self.num_groups)
+        self.up3 = Up(256, 128 // factor, self.bilinear, normalization=self.normalization, num_groups=self.num_groups)
+        self.up4 = Up(128, 64, self.bilinear, normalization=self.normalization, num_groups=self.num_groups)
+
+        # Output head on the outermost layer (full-scale output)
+        self.outc_mask = torch.nn.Sequential(OutConv(64, 64), nn.BatchNorm2d(64), nn.ReLU(inplace=True),
+                                             OutConv(64, self.n_classes))
+
+        # Hierarchically integrated intermediate output-heads
+        self.outc_x5 = torch.nn.Sequential(OutConv(1024 // factor, 64), nn.BatchNorm2d(64), nn.ReLU(inplace=True),
+                                           OutConv(64, self.n_classes))
+        self.outc_up1 = torch.nn.Sequential(OutConv(512 // factor, 64), nn.BatchNorm2d(64), nn.ReLU(inplace=True),
+                                            OutConv(64, self.n_classes))
+        self.outc_up2 = torch.nn.Sequential(OutConv(256 // factor, 64), nn.BatchNorm2d(64), nn.ReLU(inplace=True),
+                                            OutConv(64, self.n_classes))
+        self.outc_up3 = torch.nn.Sequential(OutConv(128 // factor, 64), nn.BatchNorm2d(64), nn.ReLU(inplace=True),
+                                            OutConv(64, self.n_classes))
+        self.outc_up4 = torch.nn.Sequential(OutConv(64, 64), nn.BatchNorm2d(64), nn.ReLU(inplace=True),
+                                            OutConv(64, self.n_classes))
+
+    def forward(self, x):
+        # UNet encoder
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+
+        # UNet decoder + intermediate output-heads
+        x5_logit = self.outc_x5(x5)
+
+        x = self.up1(x5, x4)
+        up1_logit = self.outc_up1(x)
+
+        x = self.up2(x, x3)
+        up2_logit = self.outc_up2(x)
+
+        x = self.up3(x, x2)
+        up3_logit = self.outc_up3(x)
+
+        x = self.up4(x, x1)
+        up4_logit = self.outc_up4(x)
+
+        x = self.dropout(x)
+
+        # Full-scale output conv
+        logit_mask_ce = self.outc_mask(x)
+
+        return logit_mask_ce, [x5_logit, up1_logit, up2_logit, up3_logit, up4_logit]
--- unet/unet_parts.py
+++ unet/unet_parts.py
@@ -4,22 +4,45 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+__all__ = ["UNet"]
+
 
 class DoubleConv(nn.Module):
     """(convolution => [BN] => ReLU) * 2"""
 
-    def __init__(self, in_channels, out_channels, mid_channels=None):
+    def __init__(self, in_channels, out_channels, mid_channels=None, dilation_rate=1, padding=1,
+                 normalization='batchnorm', num_groups=1):
         super().__init__()
         if not mid_channels:
             mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(mid_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True)
-        )
+
+        if normalization == 'batchnorm':
+            self.double_conv = nn.Sequential(
+                nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=padding, dilation=dilation_rate),
+                nn.BatchNorm2d(mid_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=padding, dilation=dilation_rate),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True)
+            )
+        elif normalization == 'groupnorm':
+            self.double_conv = nn.Sequential(
+                nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=padding, dilation=dilation_rate),
+                nn.GroupNorm(num_groups=num_groups, num_channels=mid_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=padding, dilation=dilation_rate),
+                nn.GroupNorm(num_groups=num_groups, num_channels=out_channels),
+                nn.ReLU(inplace=True)
+            )
+        elif normalization == 'no_norm':
+            self.double_conv = nn.Sequential(
+                nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=padding, dilation=dilation_rate),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=padding, dilation=dilation_rate),
+                nn.ReLU(inplace=True)
+            )
+        else:
+            raise NotImplementedError("This type of normalization: {}, was not implemented.".format(normalization))
 
     def forward(self, x):
         return self.double_conv(x)
@@ -28,11 +51,12 @@ class DoubleConv(nn.Module):
 class Down(nn.Module):
     """Downscaling with maxpool then double conv"""
 
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels, out_channels, dilation_rate=1, padding=1, normalization='batchnorm', num_groups=1):
         super().__init__()
         self.maxpool_conv = nn.Sequential(
             nn.MaxPool2d(2),
-            DoubleConv(in_channels, out_channels)
+            DoubleConv(in_channels, out_channels, dilation_rate=dilation_rate, padding=padding,
+                       normalization=normalization, num_groups=num_groups)
         )
 
     def forward(self, x):
@@ -42,16 +66,19 @@ class Down(nn.Module):
 class Up(nn.Module):
     """Upscaling then double conv"""
 
-    def __init__(self, in_channels, out_channels, bilinear=True):
+    def __init__(self, in_channels, out_channels, bilinear=True, dilation_rate=1, normalization='batchnorm',
+                 num_groups=1):
         super().__init__()
 
         # if bilinear, use the normal convolutions to reduce the number of channels
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2, dilation_rate=dilation_rate,
+                                   normalization=normalization, num_groups=num_groups)
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
+            self.conv = DoubleConv(in_channels, out_channels, dilation_rate=dilation_rate, normalization=normalization,
+                                   num_groups=num_groups)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
